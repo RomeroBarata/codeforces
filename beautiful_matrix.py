31269975412de612dd96556becaaebcf3d61ab8a
RomeroBarata <romero.barata@gmail.com>
2019-07-26 15:16:36 +1000

    Implementation for the 263A Beautiful Matrix problem.

---
+++ beautiful_matrix.py
@@ -0,0 +1,13 @@
+def main():
+    i, j = None, None
+    for i in range(1, 6):
+        row = input()
+        if '1' in row:
+            j = (row.find('1') // 2) + 1
+            break
+    min_moves = abs(3 - i) + abs(3 - j)  # Manhattan distance
+    print(min_moves)
+
+
+if __name__ == '__main__':
+    main()
